2a734e1086fce560804a1540932f13fc6574bda5
gustavomoraiss <gustavo.morais1098@gmail.com>
2022-03-27 15:53:35 -0300

    edit the splashScreen details

--- app/src/main/java/com/gmorais/weatherforecastapp/screens/WeatherSplashScreen.kt
+++ app/src/main/java/com/gmorais/weatherforecastapp/screens/WeatherSplashScreen.kt
@@ -1,10 +1,51 @@
 package com.gmorais.weatherforecastapp.screens
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.gmorais.weatherforecastapp.R
 
 @Composable
 fun WeatherSplashScreen(navController: NavController) {
-    Text("thats Splash is here")
+    androidx.compose.material.Surface(
+        modifier = Modifier
+            .padding(15.dp)
+            .size(330.dp),
+        shape = CircleShape,
+        color = Color.White,
+        border = BorderStroke(color = Color.LightGray, width = 2.dp)
+    ) {
+        Column(
+            modifier = Modifier.padding(1.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.sun),
+                contentDescription = "sunny icon",
+                contentScale = ContentScale.Fit
+            )
+            Text(
+                text = "Find the sun?",
+                style = MaterialTheme.typography.h5,
+                color = Color.LightGray
+            )
+        }
+    }
 }
